28a541c6126db5d8965d990e68038313b42805e7
Isa Turku <turkuisa00@gmail.com>
2024-09-08 23:07:02 +0200

    fix: correct categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import ProductPage from "./pages/product-page";
 const App = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  useEffect(() => navigate("/women"), [])
   const [isCartPopupOpen, setCartPopupOpen] = useState(false)
   const toggleCartPopupOpen = () => {
     setCartPopupOpen((prev) => !prev)
@@ -19,14 +18,14 @@ const App = () => {
     <>
       <header>
         <Nav toggleCartPopupOpen={toggleCartPopupOpen}>
-          <NavLink active={location.pathname === "/all"} path="/women">All</NavLink>
-          <NavLink active={location.pathname === "/clothes"} path="/men">Clothes</NavLink>
-          <NavLink active={location.pathname === "/tech"} path="/kids">Tech</NavLink>
+          <NavLink active={location.pathname === "/"} path="/">All</NavLink>
+          <NavLink active={location.pathname === "/clothes"} path="/clothes">Clothes</NavLink>
+          <NavLink active={location.pathname === "/tech"} path="/tech">Tech</NavLink>
         </Nav>
       </header >
       <main className={cn("relative px-[8vw] w-full after:z-10 after:hidden after:absolute  after:top-0 after:left-0 after:right-0 after:bg-black/45 after:h-svh", { "after:block": isCartPopupOpen })}>
         <Routes>
-          <Route path="/all" element={<ProductsPage category="all" />} />
+          <Route path="/" element={<ProductsPage category="all" />} />
           <Route path="/clothes" element={<ProductsPage category="clothes" />} />
           <Route path="/tech" element={<ProductsPage category="tech" />} />
           <Route path="/products/:id" element={<ProductPage />} />
