50f54108b1404c0f11f18c83da2b5ee6820cf513
Isa Turku <turkuisa00@gmail.com>
2024-09-08 23:30:42 +0200

    fix: link id

--- frontend/src/components/nav/link.jsx
+++ frontend/src/components/nav/link.jsx
@@ -3,7 +3,7 @@ import { cn } from "../../utils/cn"
 
 export const NavLink = ({ active, children, path }) => {
   return (
-    <Link className="uppercase cursor-pointer flex flex-col gap-4" to={path} data-test-id={active ? "active-category-link" : "category-link"}>
+    <Link className="uppercase cursor-pointer flex flex-col gap-4" to={path} data-testid={active ? "active-category-link" : "category-link"}>
       <span className={cn("px-2 font-semibold", { "text-primary": active })} >{children} </span>
       <div className={cn(`h-1 w-full `, { "bg-primary": active })}>
       </div>
